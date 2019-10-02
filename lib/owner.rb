e7b0d2cd3ed29cb6e66063a0728b8e6cd1fe8d79
Jonah Brown <jswish02@gmail.com>
2019-10-02 00:50:33 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,30 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :species
+  @@all = []
+  
+  def initialize(name)
+    @species = "human"
+    @name = name 
+    @@all << self
+  end 
+  def self.all 
+    @@all
+  end
+  def say_species
+    "I am a #{self.species}."
+  end
+  def self.count
+    self.all.count
+  end 
+  def self.reset_all
+    self.all.clear 
+  end
+  def walk_dogs
+    self.dogs.each { |dog| dog.mood = "happy"
+  end
+def walk_cats 
+  self.cats.each {|dog| cat.mood = "happy"
+ end 
+ end
+  
 end
